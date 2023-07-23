1f3f3de5fcbffe4a82c362a60c7df637f3d21f2e
ssparknt <sina.hakim.red@gmail.com>
2023-07-23 19:59:41 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import "./App.css";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const Submit = (data) => {
+    console.log({
+      FirstName: data.FirstName,
+      LastName: data.LastName,
+      Email: data.Email,
+    });
+    alert(JSON.stringify(data));
+  };
+
+  const EmailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  const FirstNameRegex = /^[a-zA-Z]{3,}$/;
+  const LastNameRegex = /^[a-zA-Z]{3,}$/;
+
+  return (
+    <>
+      <div></div>
+      <h1>react-hook-form Example</h1>
+      <div className="card">
+        <form onSubmit={handleSubmit(Submit)} className="formStyle">
+          <input
+            {...register("FirstName", {
+              validate: (value) => FirstNameRegex.test(value),
+            })}
+            placeholder="First Name"
+          />
+          {errors.FirstName && <p>Enter a valid First name!</p>}
+
+          <input
+            {...register("LastName", {
+              validate: (value) => LastNameRegex.test(value),
+            })}
+            placeholder="Last Name"
+          />
+          {errors.LastName && <p>Enter a valid Last name!</p>}
+
+          <input
+            {...register("Email", {
+              validate: (value) => EmailRegex.test(value),
+            })}
+            placeholder="Email"
+          />
+          {errors.Email && <p>Enter a valid Email address!</p>}
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+
+      <form
+        Submit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <input
+          {...register("FirstName", {
+            validate: (value) => FirstNameRegex.test(value),
+          })}
+        />
+        {/* //render input Errors */}
+        {errors.FirstName && <p>Enter a valid First name!</p>}
+      </form>
+    </>
+  );
+}
+
+export default App;
